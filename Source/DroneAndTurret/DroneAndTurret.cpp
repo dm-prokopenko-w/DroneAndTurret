950fbe4d8e02e1d3c433983a29d8b2cf70540b2a
Dmytro Prokopenko <dm.prokopenko.w@gmail.com>
2024-04-16 22:07:35 +0300

    Init project

---
+++ Source/DroneAndTurret/DroneAndTurret.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DroneAndTurret.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneAndTurret, "DroneAndTurret" );
+ 
---
+++ Source/DroneAndTurret/DroneAndTurretGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DroneAndTurretGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADroneAndTurretGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADroneAndTurretGameMode();
+};
+
+
+
